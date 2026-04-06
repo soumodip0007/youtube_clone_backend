51b78a6ddb366a7a9d8f3a8037f9f57c57023fd1
soumodip0007 <soumodipd567@gmail.com>
2026-04-06 22:02:42 +0530

    CHANGED IN USER MODEL

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,14 +46,41 @@ const userSchema = new Schema({
     }
 }, { timestamps: true })
 
-userSchema.pre("save", async function(next) {
-    if(this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10)
+userSchema.pre("save", function(next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
     next()
 })
 
 userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY 
+        }
+    )
+}
+
+userSchema.methods.generaterRefreshToken = function() {
+    jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY 
+        }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
